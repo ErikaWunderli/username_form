aff6cd29e87f226d1b4c838029b3c23f0736a170
Erika Wunderli-Vallai <vallai.erika@gmail.com>
2018-03-03 23:40:38 +0100

    Add files via upload

---
+++ handle.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = $_REQUEST['name'];
+$username = $_REQUEST['username'];
+$email = $_REQUEST['email'];
+$gender = $_REQUEST['gender'];
+$age = $_REQUEST['age'];
+$comment = $_REQUEST['comments'];
+
+echo "Name: ".$name."<br />";
+echo "Username: ".$username."<br />";
+echo "Gender: ".$gender."<br />";
+echo "Email: ".$email."<br />";
+echo "Age: ".$age."<br />";
+
+
+?>
